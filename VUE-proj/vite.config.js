94d4ac4122117b500fac42f40b80b1ab7d9ef470
Sergey <sergey@example.com>
2023-02-12 18:02:12 +0700

    0.31

--- VUE-proj/vite.config.js
+++ VUE-proj/vite.config.js
@@ -2,7 +2,7 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-
+const PORT = 3000;
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
@@ -10,5 +10,8 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  }
+  },
+  server: {
+    port: PORT,
+  },
 })
